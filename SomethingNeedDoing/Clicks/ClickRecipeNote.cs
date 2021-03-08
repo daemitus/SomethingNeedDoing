350132114cd4aee14d4d076e5b2b08ce17b72c86
Raymond Lynch <daemitus@gmail.com>
2021-03-08 17:08:37 -0500

    Add trial synthesis to click

--- SomethingNeedDoing/Clicks/ClickRecipeNote.cs
+++ SomethingNeedDoing/Clicks/ClickRecipeNote.cs
@@ -10,6 +10,7 @@ namespace SomethingNeedDoing.Clicks
         public unsafe ClickRecipeNote(SomethingNeedDoingPlugin plugin) : base(plugin)
         {
             AvailableClicks["synthesize"] = (addon) => SendClick(addon, EventType.CHANGE, 13, ((AddonRecipeNote*)addon)->SynthesizeButton->AtkComponentBase.OwnerNode);
+            AvailableClicks["trial_synthesis"] = (addon) => SendClick(addon, EventType.CHANGE, 15, ((AddonRecipeNote*)addon)->TrialSynthesisButton->AtkComponentBase.OwnerNode);
         }
     }
 }
